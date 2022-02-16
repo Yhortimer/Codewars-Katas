c78c8d513da2f55dce2fc1646553a21e384965f0
Yhortimer <igorhryplyvyy@gmail.com>
2022-02-16 21:11:54 +0200

    first commit

--- break-camelcase.js
+++ break-camelcase.js
@@ -1,3 +1,5 @@
+/* Complete the solution so that the function will break up camel casing, using a space between words. */
+
 function solution(string) {
   isupperchar = !/[a-z]/.test(string) || /[A-Z]/.test(string);
   if (isupperchar) {
